f2d2ca3e8ac791f628987e54648c20accd76001e
Ben Schwartz <schwartz.ben0@gmail.com>
2020-11-24 15:52:58 -0400

    Upped version string

--- ccxt.js
+++ ccxt.js
@@ -35,7 +35,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.38.15'
+const version = '1.38.15.1'
 
 Exchange.ccxtVersion = version
 
